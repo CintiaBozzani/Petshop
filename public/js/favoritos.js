957426b353f1cc281ac5793c23360d77c72c2aa0
CintiaBozzani <cintia.d.bozzani@gmail.com>
2022-09-06 20:16:28 -0300

    Add Favoritos

--- public/js/favoritos.js
+++ public/js/favoritos.js
@@ -1,14 +1,19 @@
-let arrayFavoritos = []
 
-function getData(){
+let arrayFavoritos = []
+const dataProducts =[]
+async function getData(){
 
 let dataLocalSt=JSON.parse(localStorage.getItem("favoritos"))
+
+await fetch("../js/dataProducts.json")
+.then(response=>response.json())
+.then(json=>dataProducts.push(...json))
 
 
 dataLocalSt.map(favorito =>{
 arrayFavoritos.push( ...dataProducts.filter(product=>product.id === favorito))
 })
-document.getElementById("QFavoritos").innerHTML=dataLocalSt.length
+//document.getElementById("QFavoritos").innerHTML=dataLocalSt.length
 
 if(window.location.pathname === "/public/paginas/Favoritos.html"){
 diplayFavoritos()
@@ -20,10 +25,19 @@ function diplayFavoritos(){
     let html=""
     arrayFavoritos.map(favorito =>{
         console.log(favorito)
-      return  html += 
-       `
-       <li>${favorito.nombre}</li>
-       ` 
+      return  html += `
+        <div class="productItem">   
+                    <img class="imgProductos" src="../imagenes/${favorito.segmento}/${favorito.imagen}" alt="producto royal canin">
+                    <div class="descripcionProducto">
+                        <h3>${favorito.marca}</h3>
+                        <h3>${favorito.nombre}</h3>
+                        <h3>${favorito.peso}Kg</h3>
+                        <h3>Precio: $${favorito.precio}</h3>
+                         
+                    </div>
+                </div>
+        `
+     
     })
 
         document.getElementById("listFavoritos").innerHTML=html
